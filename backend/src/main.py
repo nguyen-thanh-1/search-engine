e61b00ab8e97db62a12de3f04c44e2b106cc3ef2
minfus391@gmail.com <minfus391@gmail.com>
2025-11-10 15:23:55 +0700

    feat(env): add example environment configuration file

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,6 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.app_config import app_config
 from src.routes.crawling_routes import router as crawling_router
 from src.routes.search_routes import router as search_router
 from src.routes.recipes_routes import router as recipes_router
